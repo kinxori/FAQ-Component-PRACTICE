7a750eba967c0f2ae211a076a9c994e2129a1d35
kinxori <116584982+kinxori@users.noreply.github.com>
2023-03-07 23:41:13 -0600

    configuration for typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    checker({
+      typescript: true,
+    }),
+  ],
   base: "/react-practice-4"
 })
