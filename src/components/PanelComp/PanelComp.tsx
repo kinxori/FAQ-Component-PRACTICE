2073eb122a3e7ad8719aef9c7eb4962335ce64ce
kinxori <116584982+kinxori@users.noreply.github.com>
2023-03-08 00:40:13 -0600

    arrow image importation

--- src/components/PanelComp/PanelComp.tsx
+++ src/components/PanelComp/PanelComp.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import "./PanelComp.css"
+import "/src/assets/arrow-icon.png"
 
 export default function PanelComp(){
     const [character, setCharacter] = useState<{
